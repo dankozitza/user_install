0c212229eca71fcea744c169d2219e92d558af4e
dankozitza <dankoz@gmx.us>
2014-09-20 20:00:36 -0500

    changing dir structure

---
+++ userhome/.screenlayout/4-hp23-hp19-acer19-hp19wideabovecenter.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --mode 1280x1024 --pos 3200x900 --rotate normal --output DVI-1 --mode 1920x1080 --pos 0x844 --rotate normal --output DVI-0 --mode 1280x1024 --pos 1920x900 --rotate normal --output HDMI-0 --mode 1600x900 --pos 1920x0 --rotate normal
