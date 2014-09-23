7108286b3d64c66e37753baf1c774d84e3e7c9a8
Dan Kozitza <dankoz@gmx.us>
2014-09-22 20:01:06 -0500

    Delete 3-hp23-hp19-acer19.sh

--- screenlayout/3-hp23-hp19-acer19.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DisplayPort-0 --mode 1600x900 --pos 1744x0 --rotate normal --output DVI-1 --mode 1920x1080 --pos 0x900 --rotate normal --output DVI-0 --mode 1280x1024 --pos 1920x900 --rotate normal --output HDMI-0 --mode 1280x1024 --pos 3200x900 --rotate normal
