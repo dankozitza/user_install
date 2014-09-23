4a76bddaa0a0728b1e5b6c2c19ed825a0fd7f6e5
Dan Kozitza <dankoz@gmx.us>
2014-09-22 21:07:26 -0500

    testing

--- user_install.sh
+++ user_install.sh
@@ -18,51 +18,51 @@ find etc/ -maxdepth 1 -mindepth 1 -exec cp -r {} /etc/ \;
 
 ### network setup ***
 #
-#ip addr
-#
-#echo "\n!--- configure network.service ---!\n"
-#sleep 5
-#
-#vim network.service
-#
-#cp network.service /etc/systemd/system/multi-user.target.wants/
-#
-#systemctl daemon-reload
-#systemctl start network.service
-#
-#echo "\n!--- if ping fails network setup failed! ---!\n"
-#sleep 2
-#
-#ping google.com
+ip addr
+
+echo "\n!--- configure network.service ---!\n"
+sleep 5
+
+vim network.service
+
+cp network.service /etc/systemd/system/multi-user.target.wants/
+
+systemctl daemon-reload
+systemctl start network.service
+
+echo "\n!--- if ping fails network setup failed! ---!\n"
+sleep 2
+
+ping google.com
 #
 ###
 
 ### program download+installation ***
 #
-#echo "\n!--- Now going to install a few programs ---!\n"
-#sleep 2
-#
-####pacman --noconfirm -S vim
-###pacman --noconfirm -S wget
-##pacman --noconfirm -S openbox
-##pacman --noconfirm -S xorg
-##pacman --noconfirm -S xorg-xinit
-##pacman --noconfirm -S xfce4-terminal
-###pacman --noconfirm -S xfce4
-####pacman --noconfirm -S git
-####pacman --noconfirm -S go
-###pacman --noconfirm -S mpc
-###pacman --noconfirm -S ncmpcpp
-###pacman --noconfirm -S alsa-utils
-##pacman --noconfirm -S htop
-###pacman --noconfirm -S dwb
-###pacman --noconfirm -S gst-plugins-good
-####pacman --noconfirm -S dmenu
-####pacman --noconfirm -S figlet
-###pacman --noconfirm -S flashplugin
-##pacman --noconfirm -S transmission-gtk
-####pacman --noconfirm -S scrot
-####pacman --noconfirm -S gimp
+echo "\n!--- Now going to install a few programs ---!\n"
+sleep 2
+
+###pacman --noconfirm -S vim
+##pacman --noconfirm -S wget
+#pacman --noconfirm -S openbox
+#pacman --noconfirm -S xorg
+#pacman --noconfirm -S xorg-xinit
+#pacman --noconfirm -S xfce4-terminal
+##pacman --noconfirm -S xfce4
+###pacman --noconfirm -S git
+###pacman --noconfirm -S go
+##pacman --noconfirm -S mpc
+##pacman --noconfirm -S ncmpcpp
+##pacman --noconfirm -S alsa-utils
+pacman --noconfirm -S htop
+##pacman --noconfirm -S dwb
+##pacman --noconfirm -S gst-plugins-good
+###pacman --noconfirm -S dmenu
+###pacman --noconfirm -S figlet
+##pacman --noconfirm -S flashplugin
+#pacman --noconfirm -S transmission-gtk
+###pacman --noconfirm -S scrot
+###pacman --noconfirm -S gimp
 #
 ###
 
