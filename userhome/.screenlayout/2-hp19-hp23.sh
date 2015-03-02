820856afbc413d514d1e8fb77ce00b16eff5730a
Dan Kozitza <dankoz@gmx.us>
2015-03-02 02:44:58 -0600

    modified screenlayout and openbox rc

--- userhome/.screenlayout/2-hp19-hp23.sh
+++ userhome/.screenlayout/2-hp19-hp23.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --off --output DVI-1 --mode 1600x900 --pos 0x180 --rotate normal --output DVI-0 --mode 1920x1080 --pos 1600x0 --rotate normal --output HDMI-0 --off
+xrandr --output DisplayPort-0 --off --output DVI-1 --mode 1600x900 --pos 1920x180 --rotate normal --output DVI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-0 --off
