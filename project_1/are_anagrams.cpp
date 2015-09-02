89f633eb676200f0bce1d30a940c7b442d19988c
Dan Kozitza <dankoz@gmx.us>
2015-09-01 21:13:07 -0500

    finished documentation and made more cohesive

--- project_1/are_anagrams.cpp
+++ project_1/are_anagrams.cpp
@@ -3,17 +3,27 @@
 //
 // Prompts the user for two strings then checks to see if they are anagrams.
 //
+// Project 1
 // Created by Daniel Kozitza
+// CSCI 301 section 2
+// Due 09/02/2015
 //
 
-#include <iostream>
+#include <cassert>
 #include <ctype.h>
+#include <iostream>
 #include <map>
+
+const int LineMaxLength = 60;
 
 // is_lc_letter
 //
 // Returns true if the char c is a lowercase letter a-z. Otherwise returns
 // false.
+//
+// Pre-condition: A variable c of type char.
+// Post-condition: Returns a true or false value declaring whether
+//                 c is a lowercase letter or not.
 //
 bool is_lc_letter(const char c) {
 
@@ -27,14 +37,46 @@ bool is_lc_letter(const char c) {
 //
 // Sets the keys a-z to 0.
 //
+// Pre-condition: A pointer to a map with char keys and int values.
+// Post-condition: The map with key-values a-z set to 0.
+//
 void define_set(std::map<char, int> &set) {
 	for (int i = 'a'; i <= 'z'; i++)
 		set[(char) i] = 0;
 }
 
+// get_set
+//
+// Gets the set from stdin.
+//
+// Pre-condition: A pointer to a stream and a pointer to a map with char keys
+//                and int values.
+// Post-condition: The istream with one line removed and the map with keys a-z
+//                 incremented once for each a-z character from the stream.
+//
+void get_set(std::istream &in, std::map<char, int> &set) {
+	int cnt = 1;
+	while (true) {
+		char c;
+		in.get(c);
+		c = tolower(c);
+		if (c == '\n')
+			break;
+		assert(cnt < LineMaxLength);
+		cnt++;
+		if (is_lc_letter(c))
+			set[c]++;
+	}
+	assert(cnt > 1);
+}
+
 // sets_equal
 //
 // Compares keys a-z of each map.
+//
+// Pre-condition: Two maps with char keys and int values.
+// Post-condition: Returns a true or false value declaring whether the map's
+//                 keys a-z contain the same values.
 //
 bool sets_equal(std::map<char, int> set_1, std::map<char, int> set_2) {
 	for (int i = 'a'; i <= 'z'; i++) {
@@ -52,30 +94,10 @@ int main() {
 	define_set(second_set);
 
 	std::cout << "Enter the first string:  ";
-
-	// get the first set of characters
-	while (true) {
-		char c;
-		std::cin.get(c);
-		c = tolower(c);
-		if (c == '\n')
-			break;
-		if (is_lc_letter(c))
-			first_set[c]++;
-	}
+	get_set(std::cin, first_set);
 
 	std::cout << "Enter the second string: ";
-
-	// get the second set of characters
-	while (true) {
-		char c;
-		std::cin.get(c);
-		c = tolower(c);
-		if (c == '\n')
-			break;
-		if (is_lc_letter(c))
-			second_set[c]++;
-	}
+	get_set(std::cin, second_set);
 
 	if (sets_equal(first_set, second_set)) {
 		std::cout << "They are anagrams!\n";
@@ -85,27 +107,4 @@ int main() {
 		std::cout << "They are not anagrams.\n";
 		return 1;
 	}
-}
-
-void test_lc_letter() {
-
-	if (is_lc_letter('a'))
-		std::cout << "a is a letter!\n";
-	else
-		std::cout << "a is not a letter!\n";
-
-	if (is_lc_letter('z'))
-		std::cout << "z is a letter!\n";
-	else
-		std::cout << "z is not a letter!\n";
-
-	if (is_lc_letter('`'))
-		std::cout << "` is a letter!\n";
-	else
-		std::cout << "` is not a letter!\n";
-
-	if (is_lc_letter('{'))
-		std::cout << "{ is a letter!\n";
-	else
-		std::cout << "{ is not a letter!\n";
 }
