27d4a1c7b1dc477eacaf0e6632b7ef9c2ffd5418
dankozitza <dankoz@gmx.us>
2015-09-03 15:44:40 -0500

    working on ui

--- project_2/modsets.cpp
+++ project_2/modsets.cpp
@@ -24,16 +24,27 @@ void print_help() {
 	cout << "help\n";
 }
 
+void print_nonempty_sets(Set sets[]) const {
+	bool not_one = true;
+	for (int i = 0; i < MaxSets; i++) {
+		if (sets[i].size() > 0) {
+			not_one = false;
+			cout << "set number " << i << " has ";
+			cout << sets[i].size() << "elements\n";
+		}
+	}
+	if (not_one) {
+		cout << "all the sets are empty\n";
+	}
+}
+
+void 
+
 int main() {
 //	Set sets[MaxSets];
 	char cmd = 'h';
 	bool quit = false;
-	int v;
-	char c;
-	int n;
-	int i = 0;
-	int ns[MaxNs]; // this is used to store a long list of Set indexes
-	string s;
+	int v, n, n1, n2, n3;
 
 	print_help();
 	do {
@@ -42,25 +53,34 @@ int main() {
 		switch (cmd) {
 		case 'h':
 			print_help();
-
-			//getline(cin, s);
-			//cout << "s: `" << s << "`\n";
-
-			while (i < MaxNs && std::cin >> ns[i++]) {
-			//while (c != '\n') {
-				//cin >> v >> c;
-				cout << "ns[i]: `" << ns[i] << "`\n";
-				//cout << "c: `" << c << "`\n";
-			}
 			break;
-		//case 'p':
-		//	print_nonempty_sets(sets);
-		//case 'i':
-		//	insert(sets);
-		//case 'r':
-		//	remove(sets);
-		//case 'u':
-		//	set_union(sets, ns)
+		case 'p':
+			print_nonempty_sets(sets);
+			break;
+		case 'i':
+			cin >> v >> n;
+			sets[n].insert(v);
+			break;
+		case 'r':
+			cin >> v >> n;
+			sets[n].remove(v);
+			break;
+		case 'u':
+			cin >> n1 >> n2 >> n3;
+			sets[n3] = sets[n1] + sets[n2];
+			break;
+		case 's':
+			cin >> n;
+			cout << sets[n].size() << endl;
+			break;
+		case 'p':
+			cin >> v >> n;
+			print_present(sets[n], v);
+			break;
+		case 'w':
+			cin >> n;
+			cout << sets[n] << endl;
+			break;
 		case 'q':
 			quit = true;
 			break;
