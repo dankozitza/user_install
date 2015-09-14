2cb06c40eb9bb2d44555027eaff44e330617b856
dankozitza <dankoz@gmx.us>
2015-09-14 15:00:37 -0500

    started solve_hanoi.cpp

---
+++ project_3/solve_hanoi/solve_hanoi.cpp
@@ -0,0 +1,16 @@
+//
+// solve_hanoi.cpp
+//
+// takes argument n representing the number of disks being played then prints
+// a set of directions for completing the game.
+//
+// Project 3
+// Created by Daniel Kozitza
+// account number 10540413
+// CSCI 301 section 2
+// Due 09/21/2015
+//
+
+int main() {
+
+}
