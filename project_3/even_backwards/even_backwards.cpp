ce76331d66d7fe64f11c011b337bfb4f89b86c1d
dankozitza <dankoz@gmx.us>
2015-09-15 23:01:53 -0500

    working on even_backwards function

--- project_3/even_backwards/even_backwards.cpp
+++ project_3/even_backwards/even_backwards.cpp
@@ -12,20 +12,47 @@
 //
 
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
+int char_to_int(char c) {
+	// explode if c is not a number
+	assert(c >= 48 && c <= 57);
+	// convert the char in c to an int by subtracting 48
+	return c - 48;
+}
+
+void even_backwards(int n) {
+	if (n < 10) {
+		cout << "here\n";
+		if (n < 1)
+			return;
+		if (n % 2 == 0)
+			cout << n;
+		else
+			return;
+	}
+	even_backwards(n / 10);
+	if ((n % 10) % 2 == 0)
+		cout << n % 10;
+	return;
+}
+
 int main(int argc, char *argv[]) {
+	assert(argc == 2);
+
 	int n = 0, i = 0;
 	while (argv[1][i] != '\0') {
-		// explode if a number is not given
-		assert(argv[1][i] >= 48 && argv[1][i] <= 57);
-		// convert the char in argv[1][i] to an int by subtracting 48
-		n = n * 10 + argv[1][i] - 48;
+		n = n * 10 + char_to_int(argv[1][i]);
 		i++;
 	}
 
 	cout << "got integer: `" << n << "`\n";
+
+	cout << "even_backwards: `";
+	even_backwards(n);
+	cout << "`\n";
 
 	return 0;
 }
