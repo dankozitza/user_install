c957f6b1090606d2a73510d0dae5c2f21e7ba619
dankozitza <dankoz@gmx.us>
2015-09-16 15:06:10 -0500

    working on client code

--- project_3/prime_factorization/prime_factorization.cpp
+++ project_3/prime_factorization/prime_factorization.cpp
@@ -10,7 +10,35 @@
 // Due 09/21/2015
 //
 
-int main() {
+#include "iostream"
+#include "cassert"
 
+using namespace std;
 
+// char_to_int
+//
+// Converts a positive integer (0-9) in the char variable c to it's integer
+// equivalent.
+//
+// Pre-condition: A positive integer (0-9) in variable c.
+// Post-condition: Returns an integer equal to the number in c.
+//
+int char_to_int(char c) {
+	// explode if c is not a number
+	assert(c >= '0' && c <= '9');
+	// convert the char in c to an int by subtracting 48
+	return c - 48;
+}
+
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		cout << "USAGE: " << argv[0] << " INTEGER\n";
+		return 0;
+	}
+	int n = 0;
+	for (int i = 0; argv[1][i] != '\0'; i++)
+		n = n * 10 + char_to_int(argv[1][i]);
+
+	cout << "got int: `" << n << "`\n";
+	return 0;
 }
