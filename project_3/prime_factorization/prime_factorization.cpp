170b3640f86272ae840d7975b3ce90215bbf7d83
dankozitza <dankoz@gmx.us>
2015-09-17 18:47:49 -0500

    rough draft finished

--- project_3/prime_factorization/prime_factorization.cpp
+++ project_3/prime_factorization/prime_factorization.cpp
@@ -25,10 +25,31 @@ using namespace std;
 //
 int char_to_int(char c) {
 	// explode if c is not a number
-	assert(c >= '0' && c <= '9');
+	if (c < '0' || c > '9') {
+		cout << "Invalid input! positive integers only.\n";
+		throw;
+	}
 	// convert the char in c to an int by subtracting the
 	// decimal of '0' since '0' is the lowest in the range
 	return c - '0';
+}
+
+// factor
+//
+// Prints to the stream the prime factors of n.
+//
+// Pre-condition: output stream out and positive integer n
+// Post-condition: The prime factors of n written to the output stream.
+//
+void factor(ostream &out, int n) {
+	if (n > 1) {
+		int denominator = 2;
+		while (n % denominator != 0)
+			denominator++;
+
+		out << denominator << " ";
+		factor(out, n / denominator);
+	}
 }
 
 int main(int argc, char* argv[]) {
@@ -36,10 +57,11 @@ int main(int argc, char* argv[]) {
 		cout << "USAGE: " << argv[0] << " INTEGER\n";
 		return 0;
 	}
+
 	int n = 0;
 	for (int i = 0; argv[1][i] != '\0'; i++)
 		n = n * 10 + char_to_int(argv[1][i]);
 
-	cout << "got int: `" << n << "`\n";
+	factor(cout, n);
 	return 0;
 }
