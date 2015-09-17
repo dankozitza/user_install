4fdd02eff8ac2cd59f75537c6c14d451b3c0d6a9
dankozitza <dankoz@gmx.us>
2015-09-17 13:48:05 -0500

    minor changes

--- project_3/solve_hanoi/solve_hanoi.cpp
+++ project_3/solve_hanoi/solve_hanoi.cpp
@@ -1,8 +1,8 @@
 //
 // solve_hanoi.cpp
 //
-// takes argument n representing the number of disks being played then prints
-// a set of directions for completing the game.
+// takes the integer arguments, disks, source, and destination then describes
+// to the user how to move disks from source peg to destination peg.
 //
 // Project 3
 // Created by Daniel Kozitza
@@ -32,16 +32,32 @@ int char_to_int(char c) {
 	return c - '0';
 }
 
+// hanoi
+//
+// Given the number of disks, source peg and destination peg this function will
+// write out instructions for the user to move the disks from the source peg to
+// the destination peg.
+//
+// Pre-condition: Number of disks n and integers from 1 to 3 src and dest.
+// Post-condition: Instructions written to stdout.
+//
+void hanoi(int n, int src, int dest) {
+}
+
 int main(int argc, char* argv[]) {
-	if (argc != 2) {
-		cout << "USAGE: " << argv[0] << " INTEGER\n";
+	if (argc != 4) {
+		cout << "USAGE: " << argv[0] << " DISKS SOURCE DESTINATION\n";
 		return 0;
 	}
 
-	int n = 0;
+	int n = 0, s, d;
 	for (int i = 0; argv[1][i] != '\0'; i++)
 		n = n * 10 + char_to_int(argv[1][i]);
+	for (int i = 0; argv[2][i] != '\0'; i++)
+		s = s * 10 + char_to_int(argv[2][i]);
+	for (int i = 0; argv[3][i] != '\0'; i++)
+		d = d * 10 + char_to_int(argv[3][i]);
 
-	cout << "got int: `" << n << "`\n";
+	cout << "got n: " << n << " s: " << s << " d: " << d << "\n";
 	return 0;
 }
