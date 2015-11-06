27e1b890a275ec3aa1c635f291c10a59ee3a8df5
Dan Kozitza <dankoz@gmx.us>
2015-11-06 15:59:31 -0600

    started working on client

--- cs301/project_8/src/qsim.cpp
+++ cs301/project_8/src/qsim.cpp
@@ -12,13 +12,20 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <iomanip>
 #include "Q.hpp"
 
 using namespace std;
 
 typedef Q<int> Teller;
 
-int main() {
+template <class T>
+T min(T v1, T v2, T v3);
+
+template <class T>
+T cstrton(char* str);
+
+int main(int argc, char* argv[]) {
    int time = 0;
    int cust_id = 77;
    int servers;        // Number of queue/server pairs.
@@ -27,30 +34,61 @@ int main() {
    int sim_duration;   // Amount of time to run for.
    int seed;           // Seed to be used to generate random number.
 
-   cout << "Enter the number of queue/server pairs: ";
-   cin >> servers;
-   cout << "Enter the customer arrival probability (1 - 100): ";
-   cin >> cust_prob;
-   cout << "Enter the maximum transaction time (minutes): ";
-   cin >> max_trans_time;
-   cout << "Enter the simulation duration time (minutes): ";
-   cin >> sim_duration;
-   cout << "Enter a random integer: ";
-   cin >> seed;
+   if (argc == 6) {
+      servers = atoi(argv[1]);
+      cust_prob = atoi(argv[2]);
+      max_trans_time = atoi(argv[3]);
+      sim_duration = atoi(argv[4]);
+      seed = atoi(argv[5]);
+   }
+   else {
+      cout << "\nYou need to enter the variables used during the simulation.\n";
+      cout << "These values can also be entered as command line arguments.\n";
+      cout << "   ex: " << argv[0];
+      cout << " QS_PAIRS CUST_PROB MAX_TRANS_TIME SIM_DURATION SEED\n\n";
+      cout << "Enter the number of queue/server pairs: ";
+      cin >> servers;
+      cout << "Enter the customer arrival probability (1 - 100): ";
+      cin >> cust_prob;
+      cout << "Enter the maximum transaction time (minutes): ";
+      cin >> max_trans_time;
+      cout << "Enter the simulation duration time (minutes): ";
+      cin >> sim_duration;
+      cout << "Enter a random integer: ";
+      cin >> seed;
+   }
 
+   srand(seed);
    Teller *staff = new Teller[servers];
 
-   for (time; time < sim_duration; ++time) {
+   for (time; time <= sim_duration; ++time) {
+
+      cout << "\nclock: " << time << endl;
 
       if (cust_prob > rand() % 100) {
          int r_teller = rand() % servers;
          staff[r_teller].enq(cust_id++);
       }
    
-      for (int i = 0; i < servers; ++i)
-         cout << "teller " << i + 1 << ": " << staff[i] << "\n";
+      for (int i = 0; i < servers; ++i) {
+         cout << "   teller: " << right << setw(3) << i + 1;
+         cout << left << "   - wait time: ";
+         cout << right << setw(3) << "F";
+         cout << left << "   - line: " << staff[i] << "\n";
+      }
       cout << endl;
    }
 
+   delete []staff;
    return 0;
+}
+
+template <class T>
+T min (T v1, T v2, T v3) {
+   if (v1 <= v2 && v1 <= v3)
+      return v1;
+   if (v2 <= v1 && v2 <= v3)
+      return v2;
+
+   return v3;
 }
