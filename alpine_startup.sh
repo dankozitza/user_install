ab0f0b9aa05a91cdc74c73f6ed7e894d1a1d5d93
dankozitza <dankoz@gmx.us>
2021-11-23 20:33:34 +0000

    testing

--- alpine_startup.sh
+++ alpine_startup.sh
@@ -8,15 +8,16 @@ fi
 echo "Installing applications"
 #sudo apk add screen
 #sudo apk add vim
+#sudo apk add git
 #sudo apk add make
 #sudo apk add gcc
 
 if [ ! yes ]; then
-	git config --system user.name "Dan Kozitza";
+	git config --system user.name "dankozitza";
 	git config --system user.email "dankoz@gmx.us";
 	git config --system color.diff always;
 	git config --system color.status true;
-	git config --global credential.helper manager-core
+	git config --system credential.helper store;
 fi
 
 echo "Installing mechanizm includes"
