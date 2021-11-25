988877e773d8c4b4735a11e659a2a40abf11d3ea
dankozitza <dankoz@gmx.us>
2021-11-25 04:19:40 +0000

    vlc

--- alpine_startup.sh
+++ alpine_startup.sh
@@ -32,3 +32,23 @@ if [ ! yes ]; then
 echo "Installing mechanizm"
 git clone https://github.com/dankozitza/mechanizm ~/m;
 fi
+
+# xfce and vnc
+# https://www.amitnepal.com/alpine-linux-with-xfce-and-vnc-on-a-virtual-machine/
+if [ ! yes ]; then
+# uncomment community in /etc/apk/repositories
+sudo apk update
+sudo apk add setup-xorg-base xfce4 xfce4-terminal lightdm-gtk-greeter xfce4-screensaver dbus-x11 open-vm-tools-gtk faenza-icon-theme xf86-video-vmware xf86-input-mouse xf86-input-keyboard
+sudo Xorg -configure
+cp /root/xorg.conf.new /etc/X11/xorg.conf
+rc-service lightdm start
+rc-update add lightdm
+rc-update add open-vm-tools
+apk add --no-cache ca-certificates curl openssl sudo xvfb x11vnc xfce4 faenza-icon-theme bash
+# start at boot
+#rc-update add local default
+# write following to /etc/local.d/vnc.start
+##!/bin/sh
+#nohup x11vnc -xkb -noxrecord -noxfixes -noxdamage -display :0 -loop -shared -forever -bg -auth /var/run/lightdm/root/:0 -rfbport 5900 -o /var/log/vnc.log > /dev/null 2>&1 &
+#chmod +x /etc/local.d/vnc.start
+fi
